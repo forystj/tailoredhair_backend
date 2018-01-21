ec39ce04b3c57b3c091fcc7b9a0a61415986d6f5
Foryst Jankelat commit --amend --reset-author <fjankela@outlook.com>
2018-01-21 12:47:20 -0800

    password hashes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+  # These are extensions that must be enabled in order to support this database
+
 ActiveRecord::Schema.define(version: 2018_01_21_055643) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
