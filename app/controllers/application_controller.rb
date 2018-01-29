7847766e56842d08d22d9ea6f575cbf02e5b9c6e
Foryst Jankelat commit --amend --reset-author <fjankela@outlook.com>
2018-01-29 15:49:36 -0800

    cleaning up any leftover work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,18 @@ class ApplicationController < ActionController::API
   end
 
   def bearer_token
-    puts "BEARER TOKEN"
-    puts header = request.env["HTTP_AUTHORIZATION"]
-    puts "TOKEN WITHOUT BEARER"
-    puts header = request.env["HTTP_AUTHORIZATION"]
+    # puts "BEARER TOKEN"
+    # puts header = request.env["HTTP_AUTHORIZATION"]
+    # puts "TOKEN WITHOUT BEARER"
+    # puts header = request.env["HTTP_AUTHORIZATION"]
     header = request.env['HTTP_AUTHORIZATION']
     pattern = /^Bearer /
     header.gsub(pattern, '') if header && header.match(pattern)
   end
 
   def decode_token(token_input)
-    puts "DECODE TOKEN, token input: #{token_input}"
-    puts token = JWT.decode(token_input, ENV['JWT_SECRET'], true)
+    # puts "DECODE TOKEN, token input: #{token_input}"
+    # puts token = JWT.decode(token_input, ENV['JWT_SECRET'], true)
     JWT.decode(token_input, ENV['JWT_SECRET'], true)
   rescue
     render json: { status: 401, message: 'Unauthorized' }
