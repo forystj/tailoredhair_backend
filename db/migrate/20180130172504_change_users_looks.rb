8027af24a794011729ffc168c1b2b6724495361d
Foryst Jankelat commit --amend --reset-author <fjankela@outlook.com>
2018-01-30 09:39:01 -0800

    trying to fix migration

--- db/migrate/20180130172504_change_users_looks.rb
+++
@@ -1,4 +0,0 @@
-class ChangeUsersLooks < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20180130172737_dropuserslooks.rb
+++
@@ -1,5 +0,0 @@
-class Dropuserslooks < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :userlooks
-  end
-end
